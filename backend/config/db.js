f3480fdf3e366a90fab073b4d024293ec24047c0
Georges Moukoko <georgesmoukoko4@gmail.com>
2026-04-24 04:08:38 +0100

    Mise en prod

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,21 +2,26 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-    define: {
-      freezeTableName: true,
-      underscored: true,
-      timestamps: true
-    }
-  }
-);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: { require: true, rejectUnauthorized: false }
+      },
+      define: { freezeTableName: true, underscored: true, timestamps: true }
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: false,
+        define: { freezeTableName: true, underscored: true, timestamps: true }
+      }
+    );
 
 export default sequelize;
